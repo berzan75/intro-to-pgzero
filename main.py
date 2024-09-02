29230c299f23a48c9d7130af8c3bb046590d1bc6
berzan75 <issundee358@gmail.com>
2024-09-02 19:57:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,23 @@ WIDTH= 400
 HEIGHT= 400
 TITLE="BERNA"
 
+def draw():
+    width=WIDTH
+    height=HEIGHT -200
+
+    for i in range(20):
+
+        r=randint(120,201)
+        g=randint(123,255)
+        b=100
+
+        rect=Rect((0,0),(width, height))
+        rect.center=(200,200)
+        screen.draw.rect(rect,(r, g, b))
+
+
+        height+=10
+        width-= 10
+
+
 pgzrun.go()
